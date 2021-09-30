56f4c8af7edc848961a82811bbf682684b427062
DaDucking <Dorvin06@gmail.com>
2021-09-30 13:16:01 +0800

    Update Login.js
    Basic login page

--- frontend/dbs-marketplace/src/containers/Login.js
+++ frontend/dbs-marketplace/src/containers/Login.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, {useState} from "react";
 
-const Login =()=>{
-    <h2> login page</h2>
-}
+const Login = props => {
+  const initialUserState = {
+    username:"",
+    password:"",
+  };
+
+  const [user, setUser] = useState(initialUserState);
+
+  const handleInputChange = event => {
+    const { username, value } = event.target; // get the name and value of the target and update with new name/id
+    setUser({ ...user, [username]: value });
+  };
+
+  const login = () => {
+    props.login(user) //function passed in from app.js async login
+    props.history.push('/'); // go back to home
+  }
+
+  return (
+    <div className="submit-form">
+      <div>
+        <div className="form-group">
+          <label htmlFor="user">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            required
+            value={user.username}
+            onChange={handleInputChange}
+            name="name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            className="form-control"
+            password="password"
+            required
+            value={user.password}
+            onChange={handleInputChange}
+            name="password"
+          />
+        </div>
+
+        <button onClick={login} className="btn btn-success">
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
