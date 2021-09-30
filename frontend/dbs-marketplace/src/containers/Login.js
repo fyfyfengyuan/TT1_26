56881755b418dc4c654bcad5c78f36d5ba330013
DaDucking <Dorvin06@gmail.com>
2021-09-30 12:57:47 +0800

    login and dep
    Login init

--- frontend/dbs-marketplace/src/containers/Login.js
+++ frontend/dbs-marketplace/src/containers/Login.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { connect } from 'prop-types'
+import { render } from 'react-dom'
+import PropTypes from 'prop-types'
+
 
 const Login =()=>{
-    <h2> login page</h2>
-}
+
+    return(
+        <Form onSubmit={this.onSubmit}>
+            <FormGroup>
+                <Label for="username">Username</Label>
+                <Input
+                    type="username"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                    className="mb-3"
+                    onChange={this.onChange}
+                />
+                <Label for="password">Password</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    className="mb-3"
+                    onChange={this.onChange}
+                />
+                <Button
+                    color="dark"
+                    style={{marginTop: '2rem'}}
+                    block
+                >Login</Button>
+            </FormGroup>
+        </Form>
+    );
+};
 
 export default login
