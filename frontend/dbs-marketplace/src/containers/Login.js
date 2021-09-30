08dee4b0a14d1df55073e1c24fc325f1dfdbd365
DaDucking <Dorvin06@gmail.com>
2021-09-30 15:34:01 +0800

    login update

--- frontend/dbs-marketplace/src/containers/Login.js
+++ frontend/dbs-marketplace/src/containers/Login.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
-const Login = ({setLoginUser}) => {
-  const history = useHistory();
+const Login = ({setLoginUser}) => { 
+  const history = useHistory();  
   const[user,setUser] = useState({
     name:"",
     password: "",
@@ -13,48 +13,25 @@ const Login = ({setLoginUser}) => {
     setUser({...user, [name]:value});
   };
   const login =()=>{
-    axios.post("http://localhost:8000/Login",user)
+    axios.post("http://localhost:8080/Login",user)
       .then(res=> {alert(res.data.message)
         setLoginUser(res.data.user)
         history.push("/")
       })
   }
-  // const [username, setUserName] = useState();
-  // const [password, setPassword] = useState();
-
-  // async function handleLogin(credentials) {
-  //   return fetch('http://localhost:8080/login', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify(credentials)
-  //     })
-  //       .then(data => data.json())
-  // };
-
-  // const login = () => {
-  //   e.preventDefault();
-  //   const token = await handleLogin({
-  //       username,
-  //       password
-  //   });
-
-  //   history.push("/");
-  // }
 
   return (
     <div className="container">
-      <div className="d-flex p-2">
-        <div className="self-center mb-6 text-xl font-light text-gray-600">
+      <div className="row d-flex p-2" style={{ backgroundColor: "gray" }}>
+        <div className="self-center mb-6 text-xl" align="center" style={{padding:"40px","font-size":"20px","font-weight":"bold"}}>
             Login To Your Account
         </div>
-
-        <div className= "col s12" style={{padding:"20px"}}>
+      
+        <div className= "col s12" style={{padding:"10px"}}>
           <div className="submit-form">
             <div>
-              <div className="form-group row" >
-                <label htmlFor="username">Username  </label>
+              <div className="form-group row" style={{padding:"10px","font-size":"16px","font-weight":"bold"}}>
+                <label htmlFor="username">Username </label>
                 <input
                   type="text"
                   className="form-control"
@@ -66,14 +43,15 @@ const Login = ({setLoginUser}) => {
                 />
               </div>
 
-              <div className="form-group row">
-                <label htmlFor="password">Password  </label>
+              <div className="form-group row" style={{padding:"10px","font-size":"16px","font-weight":"bold"}}>
+                <label htmlFor="password">Password </label>
                 <input
                   type="text"
                   className="form-control"
+                  style={{'margin-left':"5px"}}
                   name="password"
                   required
-                  value={user.password}
+                  value={user.value}
                   onChange={handleChange}
                   name="password"
                 />
@@ -89,7 +67,7 @@ const Login = ({setLoginUser}) => {
     </div>
   );
 
-
+ 
 };
 
 export default Login;
