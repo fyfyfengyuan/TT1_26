2a48661dffbe853ea192e5f0cc65e894eae043d7
fyfyfengyuan <superfengyuan@hotmail.com>
2021-09-30 15:28:21 +0800

    login update
    return id and username

--- Backend/src/main/java/com/DBS/T26Hackathon/controller/CustomerController.java
+++ Backend/src/main/java/com/DBS/T26Hackathon/controller/CustomerController.java
@@ -3,9 +3,14 @@ package com.DBS.T26Hackathon.controller;
 
 import com.DBS.T26Hackathon.models.Customer;
 import com.DBS.T26Hackathon.services.CustomerService;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,13 +38,15 @@ public class CustomerController {
     }
     
     @GetMapping("/login")
-    public String login(String username,String password) {
+    public LinkedHashMap<String, Object> login(String username,String password) {
     	Customer user = customerService.findByUserName(username);
-    	
+    	LinkedHashMap<String, Object> rtn = new LinkedHashMap<String,Object>();
+    	rtn.put("id", user.getId());
+    	rtn.put("username", username);
     	if (user.getPassword().equals(password)) {
-    	return "Success";
+    		return rtn;
     	}else {
-    		return user.getPassword();
+    		return new LinkedHashMap<String,Object>();
     	}
   
     }
