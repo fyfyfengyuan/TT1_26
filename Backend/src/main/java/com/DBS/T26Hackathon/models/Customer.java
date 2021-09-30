b8199ff44ee5886184a7adeeef05d58a39dd1b93
fyfyfengyuan <superfengyuan@hotmail.com>
2021-09-30 15:03:03 +0800

    login dummy

--- Backend/src/main/java/com/DBS/T26Hackathon/models/Customer.java
+++ Backend/src/main/java/com/DBS/T26Hackathon/models/Customer.java
@@ -58,6 +58,70 @@ public class Customer {
     @Column(nullable = false, length = 2000)
     @NotNull
     private String gender;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getPostal_code() {
+		return postal_code;
+	}
+
+	public void setPostal_code(String postal_code) {
+		this.postal_code = postal_code;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
     
 }
 
