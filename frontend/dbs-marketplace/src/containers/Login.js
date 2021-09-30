f03f7cc0c3668584f415772600affe46194c41de
DaDucking <Dorvin06@gmail.com>
2021-09-30 14:41:08 +0800

    login update

--- frontend/dbs-marketplace/src/containers/Login.js
+++ frontend/dbs-marketplace/src/containers/Login.js
@@ -1,67 +1,95 @@
 import React, {useState} from "react";
+import axios from 'axios';
 import { useHistory } from "react-router-dom";
 
-const Login = props => { 
+const Login = ({setLoginUser}) => { 
   const history = useHistory();  
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-
-  async function handleLogin(credentials) {
-    return fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-      })
-        .then(data => data.json())
+  const[user,setUser] = useState({
+    name:"",
+    password: "",
+  });
+  const handleChange = event => {
+    const {name,value} = event.target.value;
+    setUser({...user, [name]:value});
   };
-
-  const login = async e => {
-    e.preventDefault();
-    const token = await handleLogin({
-        username,
-        password
-    });
-
-    history.push("/home");
+  const login =()=>{
+    axios.post("http://localhost:8000/Login",user)
+      .then(res=> {alert(res.data.message)
+        setLoginUser(res.data.user)
+        history.push("/")
+      })
   }
+  // const [username, setUserName] = useState();
+  // const [password, setPassword] = useState();
+
+  // async function handleLogin(credentials) {
+  //   return fetch('http://localhost:8080/login', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(credentials)
+  //     })
+  //       .then(data => data.json())
+  // };
+
+  // const login = () => {
+  //   e.preventDefault();
+  //   const token = await handleLogin({
+  //       username,
+  //       password
+  //   });
+
+  //   history.push("/");
+  // }
 
   return (
-    <div className="submit-form">
-      <div>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            required
-            value={username}
-            onChange={e => setUserName(e.target.value)}
-            name="username"
-          />
+    <div className="container">
+      <div className="d-flex p-2">
+        <div className="self-center mb-6 text-xl font-light text-gray-600">
+            Login To Your Account
         </div>
+      
+        <div className= "col s12" style={{padding:"20px"}}>
+          <div className="submit-form">
+            <div>
+              <div className="form-group row" >
+                <label htmlFor="username">Username  </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  required
+                  value={user.username}
+                  onChange={handleChange}
+                  name="username"
+                />
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            className="form-control"
-            password="password"
-            required
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            name="password"
-          />
+              <div className="form-group row">
+                <label htmlFor="password">Password  </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="password"
+                  required
+                  value={user.password}
+                  onChange={handleChange}
+                  name="password"
+                />
+              </div>
+
+              <button onClick={login} className="btn btn-success">
+                Login
+              </button>
+            </div>
+          </div>
         </div>
-
-        <button onClick={login} className="btn btn-success">
-          Login
-        </button>
       </div>
     </div>
   );
+
+ 
 };
 
 export default Login;
