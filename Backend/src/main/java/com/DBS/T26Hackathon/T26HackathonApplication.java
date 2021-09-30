25119ab8214895aa62f739af2cfc7289607ae2e0
fyfyfengyuan <superfengyuan@hotmail.com>
2021-09-30 11:32:49 +0800

    Backend init

---
+++ Backend/src/main/java/com/DBS/T26Hackathon/T26HackathonApplication.java
@@ -0,0 +1,13 @@
+package com.DBS.T26Hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26HackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26HackathonApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/DBS/T26Hackathon/T26HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBS.T26Hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T26HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
